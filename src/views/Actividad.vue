e224015b7c72e746c5f611b84b41c34d2b857f49
AndresDiazPinto <andresvis565@gmail.com>
2022-03-14 09:35:14 -0500

    Cargue AD2
    Se carga actividad didáctica # 2

--- src/views/Actividad.vue
+++ src/views/Actividad.vue
@@ -36,7 +36,7 @@
             .col-sm.mb-3.mb-sm-0
               p.fw-bold.mb-0 Arrastrar y soltar
             .col-auto
-              a.boton.boton--b(:href="obtenerLink('/actividades/story.html')" target="_blank")
+              a.boton.boton--b(:href="obtenerLink('/actividades/AD2/story.html')" target="_blank")
                 span Realizar
                 i.fas.fa-puzzle-piece
 
